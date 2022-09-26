7fa31cdb15d9beedad16a15ce54f0c8a38301285
Nikola Damjanovic <iamnikoladamjanovic@gmail.com>
2022-09-27 00:35:55 +0200

    Add support for containing colors in vertices. Use shader object to load shaders.

--- main.cpp
+++ main.cpp
@@ -2,27 +2,20 @@
 #include <glad/glad.h>
 #include <SDL2/SDL.h>
 
+#include "shader.h"
+
 const int WINDOW_W = 800;
 const int WINDOW_H = 600;
 
-const char* vertex_shader_source = "#version 330 core \n"
-                                   "layout (location = 0) in vec3 pos;\n"
-                                   "void main() { gl_Position = vec4(pos.x, pos.y, pos.z, 1.0); }";
-
-const char* fragment_shader_source = "#version 330 core \n"
-                                     "out vec4 FragColor;\n"
-                                     "void main() { FragColor = vec4(0.0f, 0.0f, 1.0f, 1.0f); }";
-
 
 int window_size_changed(void *userdata, SDL_Event *event);
-
 
 int main(int argc, char* argv[]) {
     SDL_Init( SDL_INIT_EVERYTHING );
 
     // Create SDL window
     SDL_Window* window = SDL_CreateWindow(
-        "Hello Window",
+        "Hello ZORAN",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
         WINDOW_W,
@@ -52,11 +45,11 @@ int main(int argc, char* argv[]) {
 
     // vertex object init
     float vertices[] = {
-            -0.5f, 0.5f, 0.0f,
-            -0.5f, -0.5f, 0.0f,
-            0.0f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.5f, 0.5f, 0.0f
+            -0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+            -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+            0.0f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+            0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+            0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
     };
     unsigned int indices[] = {
             0, 1, 2,
@@ -73,67 +66,29 @@ int main(int argc, char* argv[]) {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     // tell opengl how to interpret vertex data
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0 /* where to start reading data */);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0 /* where to start reading data */);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-
-
-    // vertex shader init
-    unsigned int vertex_shader {};
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_source, nullptr);
-    glCompileShader(vertex_shader);
-
-    // check if vertex shader compiled
-    int success {};
-    char info_log[512];
-    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertex_shader, 512, nullptr, info_log);
-        printf("VERTEX SHADER ERROR: %s", info_log);
-    }
-
-    // fragment shader init
-    unsigned int fragment_shader {};
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_source, nullptr);
-    glCompileShader(fragment_shader);
-
-    // check if fragment shader compiled
-    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(fragment_shader, 512, nullptr, info_log);
-        printf("FRAGMENT SHADER ERROR: %s", info_log);
-    }
-
-    // init shader program
-    unsigned int shader_program { glCreateProgram() };
-    glAttachShader(shader_program, vertex_shader);
-    glAttachShader(shader_program, fragment_shader);
-    glLinkProgram(shader_program);
-    // check if shader program linked correct
-    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shader_program, 512, nullptr, info_log);
-        printf("SHADER PROGRAM LINKING ERROR: %s", info_log);
-    }
-
-    glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
-
+   Shader shader(
+           "/home/wd-nikolad/projects/C/LearnOpenGL/shader.vs",
+           "/home/wd-nikolad/projects/C/LearnOpenGL/shader.fs"
+           );
 
     // draw loop
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     bool quit {false};
     SDL_Event e;
     while (!quit) {
-        glClearColor(0.0f, 0.2f, 0.0f, 1.0f);
+        glClearColor(1.0f, 1.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shader_program);
+        shader.use();
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
